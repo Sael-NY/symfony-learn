6a4c2da81c96f5fe76945b8a6a9ec11df1bf4acd
Salim El Hattab <hattab840@hotmail.fr>
2024-11-25 11:47:21 +0100

    Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,24 +5,23 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-// je créé une classe HomeController
+// Création de classe HomeController
 class HomeController
-{
-    // je créé une route pour ma méthode home
-    // ça veut dire que pour l'url "/",
-    // c'est ma méthode de controleur située
-    // juste dessous /qui est appélée
-    // le # est une annotation en PHP (une sorte de
-    // commentaire lu et utilisé par PHP)
+
+    // Le # est lu par PHP (commentaire like)
+    // Une url '/' est lu par le router (accueil)
     #[Route('/', 'home')]
     // je créé une méthode home
         // qui retourne une instance de la classe
         // Response (issue de Symfony)
         // la classe permet créé une réponse HTTP valide (avec un status etc)
         // et prend en parametre le HTML à envoyer au navigateur
+
+        // je crée une méthode Home et ensuite une Response qui vient du Symfony qui permet de crée une reponse HTTP.
+        // Et le HTML qui va avec
     public function home() {
         return new Response('<h1>Page Accueil</h1>');
-    }
+    };
 
 
 }
