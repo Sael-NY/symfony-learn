c0beed9c6b81b253c3d4ae54f5d10b26bb91e072
Salim El Hattab <hattab840@hotmail.fr>
2024-11-25 11:49:08 +0100

    home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,11 +11,6 @@ class HomeController
     // Le # est lu par PHP (commentaire like)
     // Une url '/' est lu par le router (accueil)
     #[Route('/', 'home')]
-    // je créé une méthode home
-        // qui retourne une instance de la classe
-        // Response (issue de Symfony)
-        // la classe permet créé une réponse HTTP valide (avec un status etc)
-        // et prend en parametre le HTML à envoyer au navigateur
 
         // je crée une méthode Home et ensuite une Response qui vient du Symfony qui permet de crée une reponse HTTP.
         // Et le HTML qui va avec
