6044b4887538df87c21baf922338442ef4e1442f
Salim El Hattab <hattab840@hotmail.fr>
2024-11-26 10:36:15 +0100

    articleID

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 // Création de classe HomeController
@@ -51,7 +52,65 @@ class ArticleController extends AbstractController {
 
         ];
 
-return $this -> render('articles.html.twig', ['articles' => $articles]);
+    return $this -> render('articles.html.twig', ['articles' => $articles]);
 }
 
-}
+    #[Route('/articles', 'article_show')]
+    public function showArticle()
+    {
+        // Instance request via le Symfony qui contient la méthode statique et
+        // ça permet de récupérer les données HTTP
+        $request = Request::createFromGlobals();
+        // Récuperer les données 'id' de GET
+        $id = $request -> get('id');
+
+        $articles = [
+            [
+                'id' => 1,
+                'title' => 'Article 1',
+                'content' => 'Content of article 1',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Article 2',
+                'content' => 'Content of article 2',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Article 3',
+                'content' => 'Content of article 3',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 4,
+                'title' => 'Article 4',
+                'content' => 'Content of article 4',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ],
+            [
+                'id' => 5,
+                'title' => 'Article 5',
+                'content' => 'Content of article 5',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+            ]
+
+        ];
+
+// je crée un article dans une variable null pour la retrouver
+    $articleFound = null;
+
+// Chaque article on lui donne un id pour enfin les retrouver et je stocke dans ma variable
+    foreach ($articles as $article) {
+    if ($article['id'] === (int)$id) {
+        $articleFound = $article;
+    }
+
+    }
+    // Je crée une reponse HTTP via le twig, render du parent AbstractController qui prend un fichier twig
+    return $this -> render('article_show.html.twig',
+        // C'est un tableau qui contient les variables articles (twig)
+        ['article' => $articleFound]);
+    }
+    }
